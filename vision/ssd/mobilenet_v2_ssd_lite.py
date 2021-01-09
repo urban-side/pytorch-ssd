18ed53f8c830039135049fb8c885feebddc5ac4a
H. Kanazawa <43166752+urban-side@users.noreply.github.com>
2021-01-09 23:05:17 +0900

    torch device setting fix

--- vision/ssd/mobilenet_v2_ssd_lite.py
+++ vision/ssd/mobilenet_v2_ssd_lite.py
@@ -60,7 +60,7 @@ def create_mobilenetv2_ssd_lite(num_classes, width_mult=1.0, use_batch_norm=True
                extras, classification_headers, regression_headers, is_test=is_test, config=config)
 
 
-def create_mobilenetv2_ssd_lite_predictor(net, candidate_size=200, nms_method=None, sigma=0.5, device=torch.device('cuda')):
+def create_mobilenetv2_ssd_lite_predictor(net, candidate_size=200, nms_method=None, sigma=0.5, device=None):
     predictor = Predictor(net, config.image_size, config.image_mean,
                           config.image_std,
                           nms_method=nms_method,
